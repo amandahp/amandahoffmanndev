2dec10e55da5fde55ec2e41f06108a52a2ae4625
amandahp <amandahoffmann96@gmail.com>
2020-12-15 16:36:46 -0300

    routes finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect  } from "react-router-dom";
 import routes from './routes/Router'
 import NotFound from './pages/Notfound/Notfound'
+
 function App() {
   return (
     <BrowserRouter>
     <div>
       <Switch>
-        <Route exact path="/"/>
+        <Route exact path="/" render={() => <Redirect to="/home" />} />
         {routes.map((entry, index) => {return (<Route key={index+1} {...entry}/>)})}
         <Route component={NotFound} />
       </Switch>
