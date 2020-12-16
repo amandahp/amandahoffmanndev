ca7aa9133600c405b39534a1ba9e7de079e9f972
amandahp <amandahoffmann96@gmail.com>
2020-12-16 00:07:51 -0300

    created home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,15 +2,30 @@ import React from 'react'
 import Header from '../../components/header/header'
 import Footer from '../../components/footer/footer'
 import styles from './Home.css'
+import logo from '../../utils/img/body-icon.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileCode } from '@fortawesome/free-solid-svg-icons'
+
 
 const Home = () => {
+    const iconCodder = <FontAwesomeIcon icon={faFileCode} className='icon-code' />
     return(
         <main>
             <div className="grid-container">
                 <div className="header"></div>
                     <Header>
                     </Header>
-                <div className="body"></div>
+                <div className="body">
+                    <div className='div-button'>
+                        <button onClick={()=> window.open('https://github.com/amandahp')} className='button-log'> {iconCodder}
+                        </button>
+                    </div>
+                    <div className='text-name'>
+                        <a href="https://www.linkedin.com/in/amanda-h-660bab130/" className='amanda-name'>Amanda Hoffmann</a>
+                        <hr className='row-first'/>
+                        <hr className='row-second'/>
+                    </div>
+                </div>
                 <div className="footer">
                     <Footer>
                     </Footer>
