ddd67d8600fddcbf24ac92faeb27c977c440dd82
amandahp <amandahoffmann96@gmail.com>
2020-12-16 15:54:04 -0300

    .

---
+++ src/pages/Notfound/Notfound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Creation from '../../pages/Creation/Creation'
+
+const NotFound = () => {
+    return(
+        <Creation>
+        </Creation>
+    )
+}
+
+export default NotFound
---
+++ src/routes/Router.js
@@ -0,0 +1,14 @@
+import Home from '../pages/Home/Home'
+import Projects from '../pages/Projects/Projects'
+import About from '../pages/About/About'
+import Contact from '../pages/Contact/Contact'
+
+const routes = [
+    { path: '/home', component:Home},
+    { path: '/about', component: About},
+    { path: '/projects', component: Projects},
+    { path: '/contact', component: Contact},
+
+]
+
+export default routes
